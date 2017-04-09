51fc7c9fd6508726ef8cff412854740631aa991a
essenlive <perchais.quentin@gmail.com>
2017-04-10 00:59:08 +0200

    bettered marmiton parser

--- server/crawl.js
+++ server/crawl.js
@@ -5,29 +5,48 @@ Meteor.startup(function () {
 
 
 Meteor.methods({
-    parse : function( func, param ){
+    scrape : function( url ){
 
-            let cheerio = require('cheerio');
-            HTTP.get( 'http://www.marmiton.org/recettes/recette_quiche-lorraine_30283.aspx', function( error, response ) {
-                if ( !error ) {
-                    var html = response.content;
-                    let $ = cheerio.load(html);
+        let cheerio = require('cheerio');
 
-                    var recette = {};
+        HTTP.get( url, function( error, response ) {
 
-                    // For each .item, we add all the structure of a company to the companiesList array
-                    // Don't try to understand what follows because we will do it differently.
-                        recette['name'] = $(".m_title").text();
-                        console.log(recette['name']);
-                        recette['description'] = $(".m_content_recette_ingredients").text();
-                        console.log(recette['description']);
+            if ( !error && response.statusCode===200 ) {
+                var html = response.content;
+                let $ = cheerio.load(html,{
+                    normalizeWhitespace: true,
+                    // xmlMode: true
+                });
 
-                        console.log(recette); // Output the data in the terminal
+                var recette = {};
 
-                }
-                else {
-                    console.log(error);
-                }
-            });
-        },
+                    recette['name'] = $(".m_title .item span").text();
+                    recette['illu'] = $(".m_content_recette_illu img").attr("src");
+                    
+                    var tags = $(".m_bloc_cadre>.m_content_recette_breadcrumb").text();
+                    recette['tags'] = tags.split(" - ");
+                    _.each(recette['tags'], function(element,index){
+                        recette['tags'][index] = element.trim();
+                        console.log(element);
+                    });
+
+
+                    recette['note'] = 0;
+                    _.each($(".m_content_recette_note img"), function(element){
+                        if (element.attribs.class==="on") {recette['note']++}
+                    });
+
+                    $(".m_content_recette_ingredients span").remove();
+                    recette['ingredients'] = $(".m_content_recette_ingredients").text().trim();
+                    recette['ingredients'] = recette['ingredients'].split("-");
+
+                    console.log(recette);
+
+
+                    }
+                    else {
+                        console.log(error);
+                    }
+                });
+    },
 })
